55bdbbcad576a516e0c290cf12914bc83365bf52
y4ell <yaael.s@gmail.com>
2024-09-22 19:04:21 +0200

    .

---
+++ src/main/java/car.java
@@ -0,0 +1,11 @@
+public class car {
+    class Car {
+        String brand;
+        String type;
+
+        public Car(String brand, String type) {
+            this.brand = brand;
+            this.type = type;
+        }
+    }
+}
